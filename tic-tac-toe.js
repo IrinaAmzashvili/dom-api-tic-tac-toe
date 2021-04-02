e45d1cd3b24008131345acfe5348bb6130a2919a
josh-willy91 <jwilly91812@yahoo.com>
2021-04-02 17:32:09 -0400

    planning game status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,25 +7,42 @@ window.addEventListener('DOMContentLoaded', event => {
         // alternate between X and O
         // if the square is already filled, nothing happens
 // Plan
-    // set variable to board element
+
+
+// set variable to board element
+const board = document.getElementById('tic-tac-toe-board');
+// player is equal to 1
+let player = 1;
+
+// create 2 image elements (X and O)
+// append image to event.target.id element
+function addImg(event) {
+    // if player is equal to 1
+    if(player === 1) {
+        let xImg = document.createElement('img');
+        xImg.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'
+        event.target.appendChild(xImg)
+        // change value of player to 2
+        player = 2
+
+        // else, change value to 1
+        } else {
+            let oImg = document.createElement('img');
+            oImg.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'
+            event.target.appendChild(oImg)
+            player = 1
+        }
+    }
+
     // add event listener listening for click on board
-        // player is equal to 1
-        // create 2 image elements (X and O)
-        // if square is already occupied
-            // don't append image or alternate turns
-        // append image to event.target.id element
-        // if player is equal to 1
-            // change value of player to 2
-            // else, change value to 1
-
-    const board = document.getElementById('tic-tac-toe-board');
-    let player = 1;
-    let xImg = document.createElement('img');
-    xImg.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'
-    let oImg = document.createElement('img');
-    oImg.scr = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'
-
+    // don't append image or alternate turns
     board.addEventListener('click', event => {
-        
+        // if square is already occupied
+        if(!event.target.src) {
+            addImg(event)
+        }
     })
+
+
+
 });
